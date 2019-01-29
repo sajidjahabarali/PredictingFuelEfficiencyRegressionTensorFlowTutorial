cb0f876c4f4e1b91ed06fdf20a54e990cf884975
Sajid Jahabar Ali <46625151+sajidjahabarali@users.noreply.github.com>
2019-01-29 16:31:25 +0000

    downloaded dataset

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PredictingFuelEfficiencyRegressionTensorFlowTutorial
+
+Using TensorFlow to predict fuel efficiency using regression in Python language.
---
+++ main.py
@@ -0,0 +1,15 @@
+
+from __future__ import absolute_import, division, print_function
+
+import pathlib
+
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+#download Auto MPG dataset
+dataset_path = keras.utils.get_file("auto-mpg.data", "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
